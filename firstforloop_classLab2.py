fd31042693a61bf9bb0c5a9645491a734a8049ba
perveen14 <97854357+perveen14@users.noreply.github.com>
2022-01-24 00:46:25 -0500

    Add files via upload

---
+++ firstforloop_classLab2.py
@@ -0,0 +1,27 @@
+#Author: Perven Chowdhury
+#Date: Jan 20, 2021
+#Simple program to show a for loop
+
+def main():
+    print("Welcome to my program!")
+    
+    #Everything indented after the for statement is the body of the loop
+    for i in range(1,11):
+        print(i, end = ".")
+        print("I Love Python!")
+
+    #always back out of the loop when you are done with it
+    print("Thank you for using my program!")
+
+    #Display "Buy" 5 times
+    for i in range(5):
+        print("Buy")
+    
+
+#call or invoke the main function 
+main()
+
+        
+        
+
+    
